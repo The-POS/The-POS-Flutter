e7626c2fb934072fb57a2ef072601a9b7339e9b8
Salah Nahed <salahamassi@users.noreply.github.com>
2021-10-28 23:57:56 +0300

    Update config.dart

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,4 +1,4 @@
 const String mainUrl = "https://stoplight.io/mocks/thepos/thepos/24265652";
 const String apiUrl = "$mainUrl/api/v1";
-const bool enableFaker = true;
+const bool enableFaker = false;
 const int productsCacheTimeIntervalInHours = 24;
