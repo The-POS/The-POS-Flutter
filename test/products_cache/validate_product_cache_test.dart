6e0ee50a4950c9205345b79b86996e3539dd4392
Salah Nahed <salahamassi@users.noreply.github.com>
2021-10-28 22:53:57 +0300

    extract makSUT helper function

--- test/products_cache/validate_product_cache_test.dart
+++ test/products_cache/validate_product_cache_test.dart
@@ -2,50 +2,39 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:thepos/features/products_feature/product_cache_policy.dart';
 
 void main() {
-  test('products cache expired on specific time interval', () {
-    const int timeIntervalInHours = 24;
-    const Duration duration = Duration(hours: timeIntervalInHours);
+  ProductCachePolicy makeSUT(
+      {required int timeIntervalInHours, required int cacheTimeOffset}) {
+    final Duration duration =
+        Duration(hours: timeIntervalInHours + cacheTimeOffset);
 
     final DateTime cacheTime = DateTime.now().subtract(duration);
 
-    final DateTime currentDateTime = DateTime.now();
+    return ProductCachePolicy(timeIntervalInHours, cacheTime);
+  }
 
-    final ProductCachePolicy cachePolicy =
-        ProductCachePolicy(timeIntervalInHours, cacheTime);
-    final bool isExpired =
-        cachePolicy.isExpired(currentDateTime: currentDateTime);
+  test('products cache expired on specific time interval', () {
+    final ProductCachePolicy sut =
+        makeSUT(timeIntervalInHours: 24, cacheTimeOffset: 0);
+
+    final bool isExpired = sut.isExpired(currentDateTime: DateTime.now());
 
     expect(isExpired, true);
   });
 
   test('products cache expired after specific time interval', () {
-    const int timeIntervalInHours = 24;
-    const Duration duration = Duration(hours: timeIntervalInHours + 1);
+    final ProductCachePolicy sut =
+        makeSUT(timeIntervalInHours: 24, cacheTimeOffset: 1);
 
-    final DateTime cacheTime = DateTime.now().subtract(duration);
-
-    final DateTime currentDateTime = DateTime.now();
-
-    final ProductCachePolicy cachePolicy =
-        ProductCachePolicy(timeIntervalInHours, cacheTime);
-    final bool isExpired =
-        cachePolicy.isExpired(currentDateTime: currentDateTime);
+    final bool isExpired = sut.isExpired(currentDateTime: DateTime.now());
 
     expect(isExpired, true);
   });
 
   test('products cache does not expired before a specific time interval', () {
-    const int timeIntervalInHours = 24;
-    const Duration duration = Duration(hours: timeIntervalInHours - 1);
+    final ProductCachePolicy sut =
+        makeSUT(timeIntervalInHours: 24, cacheTimeOffset: -1);
 
-    final DateTime cacheTime = DateTime.now().subtract(duration);
-
-    final DateTime currentDateTime = DateTime.now();
-
-    final ProductCachePolicy cachePolicy =
-        ProductCachePolicy(timeIntervalInHours, cacheTime);
-    final bool isExpired =
-        cachePolicy.isExpired(currentDateTime: currentDateTime);
+    final bool isExpired = sut.isExpired(currentDateTime: DateTime.now());
 
     expect(isExpired, false);
   });
