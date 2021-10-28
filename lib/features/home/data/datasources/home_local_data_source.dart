d2f9915dc7fd7dd2e820bb151e0ee896c4ca46e4
Salah Nahed <salahamassi@users.noreply.github.com>
2021-10-28 23:43:36 +0300

    check expired cached products before get products

--- lib/features/home/data/datasources/home_local_data_source.dart
+++ lib/features/home/data/datasources/home_local_data_source.dart
@@ -1,12 +1,20 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:thepos/core/config.dart';
 import 'package:thepos/core/init_app.dart';
 import 'package:thepos/features/home/data/models/product.dart';
+import 'package:thepos/features/products_feature/product_cache_policy.dart';
 
 class HomeLocalDataSource {
   static const String cacheTimeKey = 'cachedProductTime';
 
   Future<List<Product>> getProducts() async {
-    return productsBox.values.toList();
+    final bool isCacheExpired = await _isCacheExpired();
+    if (isCacheExpired) {
+      await productsBox.deleteFromDisk();
+      return <Product>[];
+    } else {
+      return productsBox.values.toList();
+    }
   }
 
   Future<Iterable<int>> insertProducts(List<Product> products) async {
@@ -19,5 +27,21 @@ class HomeLocalDataSource {
 
   Future<List<Product>> getProductsByGroupId(int groupId) async {
     return productsBox.values.where((pr) => pr.groupId == groupId).toList();
+  }
+
+  Future<bool> _isCacheExpired() async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+    final int? cachedProductTime = sharedPreferences.getInt(cacheTimeKey);
+    if (cachedProductTime == null) {
+      return false;
+    }
+    final DateTime cachedDateTime =
+        DateTime.fromMillisecondsSinceEpoch(cachedProductTime);
+    final ProductCachePolicy cachePolicy = ProductCachePolicy(
+      productsCacheTimeIntervalInHours,
+      cachedDateTime,
+    );
+    return cachePolicy.isExpired(currentDateTime: DateTime.now());
   }
 }
