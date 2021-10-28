bf4e45afcd40615f365c75c5451fbcbc9c48fe01
Salah Nahed <salahamassi@users.noreply.github.com>
2021-10-28 22:46:28 +0300

    products cache does not expired before a specific time interval

--- test/products_cache/validate_product_cache_test.dart
+++ test/products_cache/validate_product_cache_test.dart
@@ -32,6 +32,22 @@ void main() {
 
     expect(isExpired, true);
   });
+
+  test('products cache does not expired before a specific time interval', () {
+    const int timeIntervalInHours = 24;
+    const Duration duration = Duration(hours: timeIntervalInHours - 1);
+
+    final DateTime cacheTime = DateTime.now().subtract(duration);
+
+    final DateTime currentDateTime = DateTime.now();
+
+    final ProductCachePolicy cachePolicy =
+        ProductCachePolicy(timeIntervalInHours, cacheTime);
+    final bool isExpired =
+        cachePolicy.isExpired(currentDateTime: currentDateTime);
+
+    expect(isExpired, false);
+  });
 }
 
 class ProductCachePolicy {
