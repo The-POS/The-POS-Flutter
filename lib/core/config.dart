a7145ff26d9a7e939b4df2270b74d996d693b840
Salah Nahed <salahamassi@users.noreply.github.com>
2021-10-28 23:42:50 +0300

    store cachedProductTime in shared_preferences after insert products to local database

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,3 +1,4 @@
 const String mainUrl = "https://stoplight.io/mocks/thepos/thepos/24265652";
 const String apiUrl = "$mainUrl/api/v1";
 const bool enableFaker = true;
+const int productsCacheTimeIntervalInHours = 24;
--- lib/features/home/data/datasources/home_local_data_source.dart
+++ lib/features/home/data/datasources/home_local_data_source.dart
@@ -1,12 +1,19 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:thepos/core/init_app.dart';
 import 'package:thepos/features/home/data/models/product.dart';
 
 class HomeLocalDataSource {
+  static const String cacheTimeKey = 'cachedProductTime';
+
   Future<List<Product>> getProducts() async {
     return productsBox.values.toList();
   }
 
   Future<Iterable<int>> insertProducts(List<Product> products) async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+    sharedPreferences.setInt(
+        cacheTimeKey, DateTime.now().millisecondsSinceEpoch);
     return productsBox.addAll(products);
   }
 
