353b757e4dbef3df505db77f52c1cc9a7d7698d1
Salah Nahed <salahamassi@users.noreply.github.com>
2021-10-30 01:52:11 +0200

    store post the correct data to the end point

--- test/invoice/remote_create_invoice_test.dart
+++ test/invoice/remote_create_invoice_test.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:thepos/features/home/data/models/product.dart';
+import 'package:thepos/features/invoice/models/invoice.dart';
+import 'package:thepos/features/invoice/models/invoice_item.dart';
 import 'package:thepos/features/invoice/repositories/remote_store_invoice.dart';
 
 import '../helpers/mock_client_stub.dart';
@@ -15,5 +20,24 @@ void main() {
   test('init does not post any data to the end point', () async {
     _makeSUT();
     expect(MockClientStub.requests.isEmpty, true);
+  });
+
+  test('store post the correct data to the end point', () async {
+    final RemoteStoreInvoiceSUT sut = _makeSUT();
+    final Product testProduct =
+        Product(name: 'test name', sku: 'test sku', price: 4);
+    const int testQuantity = 2;
+
+    final Invoice testInvoice = Invoice(
+      clientId: 0,
+      items: <InvoiceItem>[
+        InvoiceItem(product: testProduct, quantity: testQuantity)
+      ],
+    );
+
+    await sut.remoteStoreInvoice.store(testInvoice.toJson());
+
+    expect(
+        MockClientStub.requests.first.body, json.encode(testInvoice.toJson()));
   });
 }
