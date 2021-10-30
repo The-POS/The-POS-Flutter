6d6220b398856a01bc8de93f228719a32c96ae72
Salah Nahed <salahamassi@users.noreply.github.com>
2021-10-30 19:00:58 +0200

    extract makeSUT helper fucntion

--- test/invoice/local_store_invoice_test.dart
+++ test/invoice/local_store_invoice_test.dart
@@ -14,39 +14,37 @@ void main() {
     await Hive.deleteFromDisk();
   });
 
-  test('retrieve delivers empty on empty cache', () async {
+  Future<LocalStoreInvoice> makeSUT() async {
     final Box<Map<String, dynamic>> hiveBox = await Hive.openBox('testBox');
-    final LocalStoreInvoice localStoreInvoice =
-        LocalStoreInvoice(hiveBox: hiveBox);
-    final List<Invoice> result = localStoreInvoice.retrieve();
+    return LocalStoreInvoice(hiveBox: hiveBox);
+  }
+
+  test('retrieve delivers empty on empty cache', () async {
+    final LocalStoreInvoice sut = await makeSUT();
+    final List<Invoice> result = sut.retrieve();
     expect(result.isEmpty, true);
   });
 
   test('retrieve delivers found invoices on non empty cache', () async {
-    final Box<Map<String, dynamic>> hiveBox = await Hive.openBox('testBox');
-    final LocalStoreInvoice localStoreInvoice =
-        LocalStoreInvoice(hiveBox: hiveBox);
+    final LocalStoreInvoice sut = await makeSUT();
 
     final Invoice invoice = anyInvoice;
-    await localStoreInvoice.store(invoice);
+    await sut.store(invoice);
 
-    final List<Invoice> result = localStoreInvoice.retrieve();
+    final List<Invoice> result = sut.retrieve();
     expect(result.isEmpty, false);
     expectInvoice(result.first, invoice);
   });
 
   test('retrieve delivers an empty cache after delete the inserted invoice',
       () async {
-    final Box<Map<String, dynamic>> hiveBox = await Hive.openBox('testBox');
-    final LocalStoreInvoice localStoreInvoice =
-        LocalStoreInvoice(hiveBox: hiveBox);
+    final LocalStoreInvoice sut = await makeSUT();
 
     final Invoice invoice = anyInvoice;
-    await localStoreInvoice.store(invoice);
+    await sut.store(invoice);
+    await sut.delete(invoice);
 
-    await localStoreInvoice.delete(invoice);
-
-    final List<Invoice> result = localStoreInvoice.retrieve();
+    final List<Invoice> result = sut.retrieve();
     expect(result.isEmpty, true);
   });
 }
