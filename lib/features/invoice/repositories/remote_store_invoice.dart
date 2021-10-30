5f73c4956dbeacde97f3264dd4794b8d630af9c3
Salah Nahed <salahamassi@users.noreply.github.com>
2021-10-30 02:22:48 +0200

    store delivers error on the client error

--- lib/features/invoice/repositories/remote_store_invoice.dart
+++ lib/features/invoice/repositories/remote_store_invoice.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:thepos/features/invoice/repositories/remote_store_invoice_error.dart';
 
 class RemoteStoreInvoice {
   RemoteStoreInvoice(this._client, this._url);
@@ -8,7 +10,13 @@ class RemoteStoreInvoice {
   final http.Client _client;
   final Uri _url;
 
-  Future<http.Response> store(Map<String, dynamic> body) {
-    return _client.post(_url, body: json.encode(body));
+  Future<http.Response> store(Map<String, dynamic> body) async {
+    try {
+      final http.Response response =
+          await _client.post(_url, body: json.encode(body));
+      return response;
+    } catch (error) {
+      return Future<http.Response>.error(RemoteStoreInvoiceErrors.connectivity);
+    }
   }
 }
--- test/invoice/remote_create_invoice_test.dart
+++ test/invoice/remote_create_invoice_test.dart
@@ -5,6 +5,7 @@ import 'package:thepos/features/home/data/models/product.dart';
 import 'package:thepos/features/invoice/models/invoice.dart';
 import 'package:thepos/features/invoice/models/invoice_item.dart';
 import 'package:thepos/features/invoice/repositories/remote_store_invoice.dart';
+import 'package:thepos/features/invoice/repositories/remote_store_invoice_error.dart';
 
 import '../helpers/mock_client_stub.dart';
 import 'helpers/remote_store_invoice_sut.dart';
@@ -16,6 +17,21 @@ void main() {
         RemoteStoreInvoice(client, Uri.http('domain', 'path'));
     return RemoteStoreInvoiceSUT(client, sut);
   }
+
+  dynamic tryStoreInvoice(
+      RemoteStoreInvoice sut, Map<String, dynamic> body) async {
+    dynamic expectedError;
+    try {
+      await sut.store(body);
+    } catch (error) {
+      expectedError = error;
+    }
+    return expectedError;
+  }
+
+  tearDown(() {
+    MockClientStub.clear();
+  });
 
   test('init does not post any data to the end point', () {
     _makeSUT();
@@ -39,5 +55,27 @@ void main() {
 
     expect(
         MockClientStub.requests.first.body, json.encode(testInvoice.toJson()));
+  });
+
+  test('store delivers error on the client error', () async {
+    final RemoteStoreInvoiceSUT sut = _makeSUT();
+    final Product testProduct =
+        Product(name: 'test name', sku: 'test sku', price: 4);
+    const int testQuantity = 2;
+
+    final Invoice testInvoice = Invoice(
+      clientId: 0,
+      items: <InvoiceItem>[
+        InvoiceItem(product: testProduct, quantity: testQuantity)
+      ],
+    );
+
+    final Exception anyException = Exception();
+    sut.client.completeWith(anyException);
+
+    final dynamic error =
+        await tryStoreInvoice(sut.remoteStoreInvoice, testInvoice.toJson());
+
+    expect(error, RemoteStoreInvoiceErrors.connectivity);
   });
 }
