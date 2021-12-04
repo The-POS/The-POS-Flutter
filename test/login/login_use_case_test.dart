2027168b1a8239e4487d5f1176ab23743b3fc88c
Salah Nahed <salahamassi@users.noreply.github.com>
2021-12-04 21:26:39 +0200

    login delivers login result 200 HTTP Response

--- test/login/login_use_case_test.dart
+++ test/login/login_use_case_test.dart
@@ -13,13 +13,43 @@ class LoginUseCaseSUT {
   final LoginUseCase loginUseCase;
 }
 
+class LoginResult {
+  LoginResult({
+    required this.token,
+    required this.user,
+    required this.expire,
+    required this.displayName,
+  });
+
+  factory LoginResult.fromJson(Map<String, dynamic> json) => LoginResult(
+        token: json['token'],
+        user: json['user'],
+        expire: json['expire'],
+        displayName: json['display_name'],
+      );
+
+  String token;
+  String user;
+  int expire;
+  String displayName;
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['token'] = token;
+    data['user'] = user;
+    data['expire'] = expire;
+    data['display_name'] = displayName;
+    return data;
+  }
+}
+
 class LoginUseCase {
   LoginUseCase(this._client, this._url);
 
   final http.Client _client;
   final Uri _url;
 
-  Future login(String username, String password) async {
+  Future<LoginResult> login(String username, String password) async {
     try {
       final http.Response response = await _client.post(
         _url,
@@ -29,14 +59,22 @@ class LoginUseCase {
         }),
       );
       if (response.statusCode == 200) {
-        return response;
+        return _tryParse(response.body);
       } else if (response.statusCode == 401) {
-        return Future.error(LoginUseCaseErrors.invalidCredential);
+        return Future<LoginResult>.error(LoginUseCaseErrors.invalidCredential);
       } else {
-        return Future.error(LoginUseCaseErrors.invalidData);
+        return Future<LoginResult>.error(LoginUseCaseErrors.invalidData);
       }
     } catch (error) {
-      return Future.error(LoginUseCaseErrors.connectivity);
+      return Future<LoginResult>.error(LoginUseCaseErrors.connectivity);
+    }
+  }
+
+  Future<LoginResult> _tryParse(String body) {
+    try {
+      return Future<LoginResult>.value(LoginResult.fromJson(jsonDecode(body)));
+    } catch (error) {
+      return Future<LoginResult>.error(LoginUseCaseErrors.invalidData);
     }
   }
 }
@@ -111,5 +149,37 @@ void main() {
       () => sut.loginUseCase.login('salahnahed', '123'),
     );
     expect(error, LoginUseCaseErrors.invalidCredential);
+  });
+
+  test('login delivers login result 200 HTTP Response', () async {
+    final LoginUseCaseSUT sut = _makeSUT();
+
+    const String anyToken = 'token';
+    const String user = 'alaadiaa';
+    const int expire = 1640366209;
+    const String displayName = 'Alaa Alsalehi';
+
+    final LoginResult expectedResult = LoginResult(
+      token: anyToken,
+      user: user,
+      expire: expire,
+      displayName: displayName,
+    );
+    const String response =
+        '{"token": "$anyToken","user": "$user","expire": $expire,"display_name": "$displayName"}';
+
+    const String username = 'salahnahed';
+    const String password = '123';
+
+    sut.client
+        .completeWithResponse(MockClientStub.createResponse(200, response));
+
+    final LoginResult actualResult =
+        await sut.loginUseCase.login(username, password);
+
+    expect(actualResult.token, expectedResult.token);
+    expect(actualResult.user, expectedResult.user);
+    expect(actualResult.expire, expectedResult.expire);
+    expect(actualResult.displayName, expectedResult.displayName);
   });
 }
