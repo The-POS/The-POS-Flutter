463c40bde794bafd635fc39cc149725487ecee51
Salah Nahed <salahamassi@users.noreply.github.com>
2021-12-04 20:41:00 +0200

    login post the correct data to the login end point

--- test/login/login_use_case_test.dart
+++ test/login/login_use_case_test.dart
@@ -1,20 +1,37 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 
 import '../helpers/mock_client_stub.dart';
+import '../invoice/helpers/shared_test_helper.dart';
 
 class LoginUseCase {
-  LoginUseCase(this._client);
+  LoginUseCase(this._client, this._url);
 
   final http.Client _client;
+  final Uri _url;
 
-  void login() {}
+  Future login(String username, String password) async {
+    await _client.post(
+      _url,
+      body: json.encode(<String, String>{
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    return Future.value('');
+  }
 }
 
 void main() {
   LoginUseCase _makeSUT() {
     final MockClientStub client = MockClientStub();
-    final LoginUseCase sut = LoginUseCase(client);
+    final LoginUseCase sut = LoginUseCase(
+      client,
+      Uri.http('domain', 'path'),
+    );
     return sut;
   }
 
@@ -25,5 +42,21 @@ void main() {
   test('init does not post any data to the login end point', () {
     _makeSUT();
     expect(MockClientStub.requests.isEmpty, true);
+  });
+
+  test('login post the correct data to the login end point', () async {
+    final LoginUseCase sut = _makeSUT();
+
+    const String username = 'salahnahed';
+    const String password = '123';
+
+    await tryFunction(() => sut.login(username, password));
+
+    expect(
+        MockClientStub.requests.first.body,
+        json.encode(<String, String>{
+          'username': username,
+          'password': password,
+        }));
   });
 }
