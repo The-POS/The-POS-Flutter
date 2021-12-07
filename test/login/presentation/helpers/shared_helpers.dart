9b7a57465d0e5e3a5c8027491ab514060a353028
Salah Nahed <salahamassi@users.noreply.github.com>
2021-12-08 01:53:20 +0200

    extract anyLoginResult to shared_helper

---
+++ test/login/presentation/helpers/shared_helpers.dart
@@ -0,0 +1,8 @@
+import 'package:thepos/features/login/data/models/login_result.dart';
+
+final LoginResult anyLoginResult = LoginResult(
+  token: 'token',
+  user: 'user',
+  expire: 0,
+  displayName: 'displayName',
+);
--- test/login/presentation/login_router_test.dart
+++ test/login/presentation/login_router_test.dart
@@ -1,22 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:thepos/features/login/data/models/login_result.dart';
 import 'package:thepos/features/login/presentation/login_router.dart';
 import 'package:thepos/routes/mobile_app_pages.dart';
 
 import '../../helpers/navigator/navigator_factory_spy.dart';
 import '../../helpers/navigator/navigator_shared_helper.dart';
+import 'helpers/shared_helpers.dart';
 
 Future<void> main() async {
   test('router should offAndToNamed to the home route on success login', () {
     final NavigatorFactorySpy navigatorFactory = NavigatorFactorySpy();
     final LoginRouter sut = LoginRouter(navigatorFactory);
 
-    final LoginResult anyLoginResult = LoginResult(
-      token: 'token',
-      user: 'user',
-      expire: 0,
-      displayName: 'displayName',
-    );
     sut.onLoginSuccess(anyLoginResult);
 
     final Route expectedRoute =
