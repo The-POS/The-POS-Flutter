b7eb5ce874cc1c5e98efe94826196a1280f63fdc
Salah Nahed <salahamassi@users.noreply.github.com>
2021-12-08 01:53:46 +0200

    loading rx property should be false on onLoginSuccess called

--- test/login/presentation/login_controller_test.dart
+++ test/login/presentation/login_controller_test.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:thepos/features/login/data/login_service/login_errors.dart';
+import 'package:thepos/features/login/data/login_use_case/login_use_case_output.dart';
+import 'package:thepos/features/login/data/models/login_result.dart';
 
-class LoginController extends GetxController {
+import 'helpers/shared_helpers.dart';
+
+class LoginController extends GetxController implements LoginUseCaseOutput {
   RxBool loading = RxBool(false);
 
   void login() {
@@ -12,6 +17,14 @@ class LoginController extends GetxController {
   void onClose() {
     loading.value = false;
     super.onClose();
+  }
+
+  @override
+  void onLoginFail(LoginErrors error) {}
+
+  @override
+  void onLoginSuccess(LoginResult result) {
+    loading.value = false;
   }
 }
 
@@ -36,6 +49,18 @@ void main() {
     sut.loading.value = true;
 
     await Get.delete<LoginController>();
+
+    expect(sut.loading.value, false);
+  });
+
+  test('loading rx property should be false on onLoginSuccess called',
+      () async {
+    final LoginController sut = LoginController();
+    Get.put(sut);
+
+    expect(sut.loading.value, false, reason: 'precondition failed');
+
+    sut.onLoginSuccess(anyLoginResult);
 
     expect(sut.loading.value, false);
   });
