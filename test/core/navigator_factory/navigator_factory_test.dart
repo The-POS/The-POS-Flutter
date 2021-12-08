ad0257092df26ee5a08268305df6e416f2c47922
Salah Nahed <salahamassi@users.noreply.github.com>
2021-12-08 02:34:32 +0200

    fix typo

--- test/core/navigator_factory/navigator_factory_test.dart
+++ test/core/navigator_factory/navigator_factory_test.dart
@@ -4,10 +4,10 @@ import 'package:thepos/core/navigator/app_navigator_factory.dart';
 
 import 'helpers.dart';
 
+/// ideally we should decouple our tests from Get implementation,
+///  maybe the better approach is to use navigatorObservers
+///  instead of Get.previousRoute && Get.currentRoute **/
 void main() {
-  /** ideally we should decouple our tests from Get implementation,
-   *  maybe the better approach is to use navigatorObservers
-   *  instead of Get.previousRoute && Get.currentRoute **/
   testWidgets(
     'offAndToNamed should pop the current widget and pushed the new one',
     (WidgetTester tester) async {
@@ -16,7 +16,7 @@ void main() {
       expect(Get.currentRoute, AnyRoutes.ANY_ROUTE, reason: 'precondition');
 
       final AppNavigatorFactory sut = AppNavigatorFactory();
-      sut.offAndToNamed(AnyRoutes.ANY_ROUTE);
+      sut.offAndToNamed(AnyRoutes.ANOTHER_ROUTE);
 
       expect(Get.previousRoute, '',
           reason: 'offAndToNamed should remove previousRoute from the stack');
