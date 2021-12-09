c92b5c38adde40a40499724ed6fa60203cdafaa0
Salah Nahed <salahamassi@users.noreply.github.com>
2021-12-10 00:24:10 +0200

    add splash view to mobile routes

--- lib/routes/mobile_app_pages.dart
+++ lib/routes/mobile_app_pages.dart
@@ -3,10 +3,15 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:thepos/features/carts/presentation/views/mobile/cart_view.dart';
 import 'package:thepos/features/home/presentation/views/mobile/home_view.dart';
 import 'package:thepos/features/login/presentation/views/login_view.dart';
+import 'package:thepos/features/splash/presentation/views/splash_view.dart';
 
 const String MOBILE_INITIAL = MobileRoutes.HOME;
 
 final List<GetPage<Widget>> mobileRoutes = <GetPage<Widget>>[
+  GetPage<SplashView>(
+    name: MobileRoutes.SPLASH,
+    page: () => SplashView(),
+  ),
   GetPage<LoginView>(
     name: MobileRoutes.LOGIN,
     page: () => const LoginView(),
@@ -22,6 +27,7 @@ final List<GetPage<Widget>> mobileRoutes = <GetPage<Widget>>[
 ];
 
 abstract class MobileRoutes {
+  static const String SPLASH = '/mobile_splash';
   static const String LOGIN = '/mobile_login';
   static const String HOME = '/mobile_home';
   static const String CART = '/mobile_cart';
