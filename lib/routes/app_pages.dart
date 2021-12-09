45d60b42a8c283144f3224ba9fd0859d4587cb93
Salah Nahed <salahamassi@users.noreply.github.com>
2021-12-10 00:53:41 +0200

    initial is alway splash

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -4,6 +4,6 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:thepos/routes/mobile_app_pages.dart';
 import 'package:thepos/routes/web_app_pages.dart';
 
-final String initial = kIsWeb ? WebRoutes.SPLASH : MobileRoutes.HOME;
+final String initial = kIsWeb ? WebRoutes.SPLASH : MobileRoutes.SPLASH;
 
 final List<GetPage<Widget>> routes = kIsWeb ? webRoutes : mobileRoutes;
